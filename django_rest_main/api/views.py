6c7bbcf0e9e7e39f192209094a97ab963a77d402
Balaji45-Ncr <ncrlpb45@gmail.com>
2025-05-16 22:46:32 +0530

    method based serializer

--- django_rest_main/api/views.py
+++ django_rest_main/api/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from django.core import serializers
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+@api_view(['GET','POST'])
 def studentsView(request):
-    students={'id': 1, 'name': 'John Doe', 'age': 25 }
-    return JsonResponse(students)
+    if request.method=='GET':
+    # try:
+        students=Student.objects.all()
+        #a=serializers.serialize(students)
+        a=StudentSerializer(students,many=True)
+        return Response(a.data,status=status.HTTP_200_OK)
+    # except TypeError:
+    #     return HttpResponse('<h1>sorry type error</h1>')
+    elif request.method=='POST':
+        serializer=StudentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
